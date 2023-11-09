70300315ad23516c0ccde5daac7bdaa0be0cd4ad
tgibbons-css <tgibbons@css.edu>
2023-11-09 13:05:35 -0600

    Small updates

--- lib/main.dart
+++ lib/main.dart
@@ -21,67 +21,64 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   List<Pizza> pizzasInOrder = [];
   final pizzaToppings = TextEditingController();
   int sizeSelected = 1;
 
   void _addPizza() {
-      print("addPizza called -- size = "+pizzasInOrder.length.toString());
-      showDialog<String>(
-          context: context,
-          builder: (BuildContext context) =>
-             Dialog(
-                child:Column(
-                    children: [
-                      Text("Order your pizza"),
-                      TextField(controller: pizzaToppings ),
-                      StatefulBuilder(
-                        builder: (BuildContext context, StateSetter dropDownState) {
-                          return DropdownButton(
-                            style: Theme
-                                .of(context)
-                                .textTheme
-                                .headline4,
-                            value: sizeSelected,
-                            items: [
-                              DropdownMenuItem(child: Text("Small"), value: 0),
-                              DropdownMenuItem(child: Text("Medium"), value: 1),
-                              DropdownMenuItem(child: Text("Large"), value: 2),
-                              DropdownMenuItem(child: Text("X-Lareg"), value: 3)
-                            ],
-                            onChanged: (value) {
-                              setState(() {
-                                dropDownState(() {
-                                  sizeSelected = value!;
-                                  sizeSelected = value!;
-                                });
-                              });
-                            }
-                          );
-                        }
-                      ),
-                  ElevatedButton(
-                    child: Text("Place Pizza Order"),
-                    onPressed: () {
-                      setState(() {
-                        pizzasInOrder.add( Pizza(pizzaToppings.text,sizeSelected));
-                        Navigator.pop(context);
-                      });;
-                    }
-                    ),
-                    ],
-                  )
-              )
-      );
-
-
+    print("addPizza called -- size = " + pizzasInOrder.length.toString());
+    showDialog<String>(
+      context: context,
+        builder: (BuildContext context) => Dialog(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,     // shrink the dialog down to a small size
+            children: [
+            Text("Build your pizza"),
+            TextField(
+              controller: pizzaToppings,
+              textAlign: TextAlign.center,
+            ),
+            StatefulBuilder(
+                builder: (BuildContext context, StateSetter dropDownState) {
+              return DropdownButton(
+                style: Theme.of(context).textTheme.headline4,
+                value: sizeSelected,
+                items: [
+                  DropdownMenuItem(child: Text("Small"), value: 0),
+                  DropdownMenuItem(child: Text("Medium"), value: 1),
+                  DropdownMenuItem(child: Text("Large"), value: 2),
+                  DropdownMenuItem(child: Text("X-Lareg"), value: 3)
+                ],
+                onChanged: (value) {
+                    dropDownState(() {
+                      sizeSelected = value!;
+                    });
+                  });
+              }),
+                SizedBox(height: 50),
+                ElevatedButton(
+                  child: Text("Place Pizza Order"),
+                  onPressed: () {
+                    setState(() {
+                      pizzasInOrder
+                          .add(Pizza(pizzaToppings.text, sizeSelected));
+                      Navigator.pop(context);
+                    });
+                    ;
+                  }),
+                SizedBox(height: 50),
+              ],
+            )
+        )
+    );
   }
 
   @override
@@ -92,14 +89,16 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
 
       body: ListView.builder(
-          itemCount: pizzasInOrder.length,
-          itemBuilder: (BuildContext context, int position) {
-            return ListTile(
-                title: Text(pizzasInOrder[position].description),
-                leading: Icon(Icons.local_pizza),
-            );
-          }
-      ),
+        itemCount: pizzasInOrder.length,
+        itemBuilder: (BuildContext context, int position) {
+          return Card(
+            color: Colors.blue[50],
+            child: ListTile(
+              title: Text(pizzasInOrder[position].description),
+              leading: Icon(Icons.local_pizza),
+            ),
+          );
+        }),
 
       floatingActionButton: FloatingActionButton(
         onPressed: _addPizza,
@@ -120,6 +119,6 @@ class Pizza {
 
   Pizza(String this.toppings, int this.size) {
     price = PIZZA_PRICES[size];
-    description = PIZZA_SIZES[size] + " pizza with "+toppings;
+    description = PIZZA_SIZES[size] + " pizza with " + toppings;
   }
 }
